dd25db03fce00a3738c63c7896e54d32d51db04d
tesfaye0413 <tesfaye0413@gmail.com>
2024-10-03 16:05:05 -0400

    added helloworld to week_one repo

---
+++ helloworld/src/com/revature/Launcher.java
@@ -0,0 +1,20 @@
+package com.revature;
+
+public class Launcher {
+
+//    "main" method - starting or entering point
+    public static void main(String[] args) {
+        //This is a print statement. it allows us to "print" a message to the console
+        System.out.println("Hello World!");
+        System.out.println("We are learning fundamental java!");
+        System.out.println("We can type \"sout\" & hit enter to quickly generate a print statement!");
+        System.out.println("We can type 'main' and hit enter to quickly generate the main method!");
+
+        /*
+         * This is a block comment AKA multi-line comment
+         * System = this system. the console
+         * out = output, we're defining sth that will be outputed.
+         * println = print a line to the console.
+         */
+    }
+}
